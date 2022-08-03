24c4702f2ab95ddb9b9efa66a2f3212d0192fd6b
CodingJiyoung <jiyoung.yoon11@gmail.com>
2022-08-02 22:20:28 -0400

    section1 updated

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return 'Hello world!'
+    return 'Hello world!!!'
+
+@app.route('/about')
+def about():
+    return "this is about page"
+    
+
+if __name__ == "__main__":
+    app.run(debug = True, host = "0.0.0.0", port = 3000)
